cbe107a27736a524cf0571c3e7052735dbdb210b
santiago.molina@noatec.co <santiago.molina@noatec.co>
2021-11-09 17:28:46 -0500

    Added Queries to get the version to allow update Mutations. Added Outputs management

--- IotToAppSyncLambda/index.js
+++ IotToAppSyncLambda/index.js
@@ -1,7 +1,7 @@
 const { createAuthLink } = require("aws-appsync-auth-link");
 const { createSubscriptionHandshakeLink } = require("aws-appsync-subscription-link");
 
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 const { ApolloLink } = require("apollo-link");
 const { createHttpLink } = require("apollo-link-http");
 const apolloClient = require("@apollo/client/core");
@@ -23,103 +23,149 @@ const auth = {
 
 const httpLink = createHttpLink({ uri: url, fetch: fetch });
 
-const link = ApolloLink.from([
-  createAuthLink({ url, region, auth }),
-  createSubscriptionHandshakeLink({ url, region, auth }, httpLink)
-]);
+const link = ApolloLink.from([createAuthLink({ url, region, auth }), createSubscriptionHandshakeLink({ url, region, auth }, httpLink)]);
 
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
 });
 
-const LIST_LAB_PRACTICES = gql`
-query listLabPractices {
-  listLabPractices{
-    items{
-      id,
-      name
+//---GQL Queries---
+const QUERY_OUTPUT = gql`
+  query getLabPracticeSessionOutput($id: ID!) {
+    getLabPracticeSessionOutput(id: $id) {
+      id
+      _version
     }
   }
-  }
-  `;
-
-const UPDATE_COMMAND = gql`
-mutation updateLabPracticeSessionCommand(
-  $id: ID!
-  $status: String!
-  ) {
-  updateLabPracticeSessionCommand(input: {id: $id status: $status}){
-    id,
-    status
-  }
-}
 `;
 
-// exports.handler = async (event) => {
-//   switch (event.type) {
-//     case "output":
-//       break;
-//     case "command":
-//       try {
-//         const ans = await client.mutate({
-//           mutation: UPDATE_COMMAND,
-//           variables: {
-//             Command: {
-//               id: event.uuid,
-//               status: "ok"
-//             }
-//           }
-//         })
-//         console.log(ans)
-//       } catch (error) {
-//         console.log(error)
-//       }
-//       break;
-//   }
-//   try {
-//     const Test = await client.query({
-//       query: LIST_LAB_PRACTICES
-//     })
-//     console.log(Test.data.listLabPractices.items)
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-
-main();
-
-async function main() {
-  let event = {
-    type: "command",
-    uuid: "49b9fd50-ad16-4738-9132-f96cac186cc3",
-    status: "fail"
+const UPDATE_OUTPUT = gql`
+  mutation updateLabPracticeSessionOutput($id: ID!, $value: String!, $version: Int!, $captureDate: AWSDateTime!, $storageDate: AWSDateTime!) {
+    updateLabPracticeSessionOutput(input: { id: $id, value: $value, _version: $version, captureDate: $captureDate, storageDate: $storageDate }) {
+      id
+      value
+      _version
+      captureDate
+    }
   }
-  switch (event.type) {
-    case "output":
-      break;
-    case "command":
-      try {
-        const ans = await client.mutate({
-          mutation: UPDATE_COMMAND,
-          variables: {
-            id: event.uuid,
-            status: event.status
-          }
-        })
-        console.log(ans)
-      } catch (error) {
-        console.log(error)
-      }
-      break;
+`;
+
+const QUERY_COMMAND = gql`
+  query getLabPracticeSessionCommand($id: ID!) {
+    getLabPracticeSessionCommand(id: $id) {
+      id
+      _version
+    }
   }
+`;
+
+const UPDATE_COMMAND = gql`
+  mutation updateLabPracticeSessionCommand($id: ID!, $status: String!, $version: Int!, $executionDate: AWSDateTime!) {
+    updateLabPracticeSessionCommand(input: { id: $id, status: $status, _version: $version, executionDate: $executionDate }) {
+      id
+      status
+      _version
+      executionDate
+    }
+  }
+`;
+//---------------
+
+async function updateLabPracticeSessionCommand(uuid, status, executionDate) {
   try {
-    const Test = await client.query({
-      query: LIST_LAB_PRACTICES
-    })
-    console.log(Test.data.listLabPractices.items)
+    //Query labPracticeSessionCommand to get version information (See AppSync version control)
+    const queryAns = await client.query({
+      query: QUERY_COMMAND,
+      variables: { id: uuid },
+    });
+
+    const commandData = queryAns.data.getLabPracticeSessionCommand;
+    console.log(commandData);
+
+    //Update labPracticeSessionCommand
+    try {
+      const mutationAns = await client.mutate({
+        mutation: UPDATE_COMMAND,
+        variables: {
+          id: uuid,
+          status: status,
+          version: commandData._version,
+          executionDate: executionDate,
+        },
+      });
+      console.log(mutationAns.data.updateLabPracticeSessionCommand);
+    } catch (error) {
+      console.log(error);
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
+async function updateLabPracticeSessionOutput(uuid, value, captureDate) {
+  try {
+    //Query labPracticeSessionOutput to get version information (See AppSync version control)
+    const queryAns = await client.query({
+      query: QUERY_OUTPUT,
+      variables: { id: uuid },
+    });
+
+    const outputData = queryAns.data.getLabPracticeSessionOutput;
+    console.log(outputData);
+
+    //Update labPracticeSessionOutput
+    try {
+      const mutationAns = await client.mutate({
+        mutation: UPDATE_OUTPUT,
+        variables: {
+          id: uuid,
+          value: value,
+          version: outputData._version,
+          captureDate: captureDate
+        },
+      });
+      console.log(mutationAns.data.updateLabPracticeSessionOutput);
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.handler = async (event) => {
+  switch (event.type) {
+    case "output":
+      updateLabPracticeSessionOutput(event.uuid, event.value, event.captureDate);
+      break;
+    case "command":
+      updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
+      break;
+  }
+};
+
+// main();
+
+// async function main() {
+//   let event = {
+//     type: "output",
+//     uuid: "c78ac980-186d-4b89-9347-b77f3f771346",
+//     value: "Hey",
+//     captureDate: "2021-11-09T17:16:00-05:00",
+//   };
+//   // let event = {
+//   //   type: "command",
+//   //   uuid: "49b9fd50-ad16-4738-9132-f96cac186cc3",
+//   //   status: "Hi",
+//   //   executionDate: "2021-11-09T16:40:00-05:00",
+//   // };
+//   switch (event.type) {
+//     case "output":
+//       updateLabPracticeSessionOutput(event.uuid, event.value, event.captureDate);
+//       break;
+//     case "command":
+//       updateLabPracticeSessionCommand(event.uuid, event.status, event.executionDate);
+//       break;
+//   }
+// }
